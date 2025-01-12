3898841176180e425d1e8f97e97a3ab31bbc863b
Leonardo Berbert <leo4berbert@gmail.com>
2025-01-12 19:46:52 -0300

    Update generate_hyprpaper_config.sh

--- usr/local/bin/generate_hyprpaper_config.sh
+++ usr/local/bin/generate_hyprpaper_config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Path to the wallpaper
-WALLPAPER="/usr/share/backgrounds/community/BigHypr001.jpg"
+WALLPAPER="/usr/share/backgrounds/community/animal-hpr-001.jpg"
 
 # Check if the wallpaper file exists
 if [ ! -f "$WALLPAPER" ]; then
@@ -21,6 +21,7 @@ echo "preload = $WALLPAPER" > "$CONFIG_FILE"
 # Detect active monitors and add wallpaper configuration for each one
 hyprctl monitors | grep -E "^Monitor" | awk '{print $2}' | while read -r monitor; do
     echo "wallpaper = $monitor,$WALLPAPER" >> "$CONFIG_FILE"
+    echo "wallpaper_scale = $monitor,fit" >> "$CONFIG_FILE"  # Add wallpaper_scale for each monitor
 done
 
 echo "Hyprpaper config generated at $CONFIG_FILE"
